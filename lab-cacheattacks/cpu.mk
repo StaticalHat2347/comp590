b08710aacd0414e9e9d2ff7aa474974ba3cc87cf
stats <riangg2004@gmail.com>
2026-02-27 20:28:04 -0500

    adding cpu.mk

--- lab-cacheattacks/cpu.mk
+++ lab-cacheattacks/cpu.mk
@@ -1,11 +1,4 @@
 CC=gcc
 CFLAGS=-O0 -I /usr/local
-<<<<<<< HEAD
-# Replace with your assigned CPUs from the lab email if different.
-SENDER_CPU=0
-RECEIVER_CPU=1
-=======
 SENDER_CPU=9
 RECEIVER_CPU=25
-
->>>>>>> 727d4cfb86dea4b18d06d5937c63879ea0c9f188
